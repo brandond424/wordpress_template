668a05d8f87e8cb90329aed8814bc5e181ac9559
Brandon Diaz <brandondiaz@IntMac-R0D2G8WL.local>
2018-01-02 00:59:27 -0500

    secured wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -17,23 +17,14 @@ define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
 // Salts  @{ https://api.wordpress.org/secret-key/1.1/salt/ } //
-define('AUTH_KEY',         'ls5+}?=s~2QZeDJ+*Cb47Dg};|:Cw&iGJW3h?`K+^zm5jtfh8xlSJzS?y#Rn`<8/');
-define('SECURE_AUTH_KEY',  '`eD7TGMdtf<0m%`)vu4gOtn77|fS6LZdJ3-j#++I6F<,DGxC%%{:va8&fdmK&u(K');
-define('LOGGED_IN_KEY',    'Axd.5NXkhdT`X|]+</Vfiw89BU!hucG=M5ru-%]VT0!X@8`Jm1|9--]t6J?j=^qM');
-define('NONCE_KEY',        '!a?{A](:ut%2tV}ROAd3C-?x(R9,y;~idCS}C-oRy3$>j69yaP_XY4Yr/9VtjMW^');
-define('AUTH_SALT',        'B&p{mi+oXSoApE|h~^XPL~11s^m>R-E3BTM^6/[.Ib<H<VGJ}Q9$2o@9==|l]z;c');
-define('SECURE_AUTH_SALT', '?gW9JaMM.Ai~wMF>oK.x<!H-*9+m t=e3|aMnNuWsgi E$Aru6)+=T]]F# TTcmx');
-define('LOGGED_IN_SALT',   '9!b!tWB_9 ^*p!Bw15Ah8.)pky-T]{8A3WH|(%-4S:Jh7p]zC<-0</r9f5$yW ve');
-define('NONCE_SALT',       'c`v&71Db^_VdFsL.KOm5c[Z8^}WewIt2EzG!p=Eyi8`Q{@|oC8PC$=-HkPUVXHUR');
-
-// define('AUTH_KEY',         'put your unique phrase here');
-// define('SECURE_AUTH_KEY',  'put your unique phrase here');
-// define('LOGGED_IN_KEY',    'put your unique phrase here');
-// define('NONCE_KEY',        'put your unique phrase here');
-// define('AUTH_SALT',        'put your unique phrase here');
-// define('SECURE_AUTH_SALT', 'put your unique phrase here');
-// define('LOGGED_IN_SALT',   'put your unique phrase here');
-// define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
 
 // Content Directories //
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/app' );
@@ -44,7 +35,24 @@ define( 'WPMU_PLUGIN_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/app/mu-plugins'
 // DB Prefix //
 $table_prefix  = 'wp_';
 
+// PHP Memory Limit (may need to be defined on server)
+define('WP_MEMORY_LIMIT', '128M');
+
+// Server File Permissions
+define('FS_CHMOD_FILE', 0644);
+define('FS_CHMOD_DIR', 0755);
+
+// WP Debug Mode
 define('WP_DEBUG', false);
+if (WP_DEBUG) {
+	define( 'WP_DEBUG_LOG', true);
+	define( 'WP_DEBUG_DISPLAY', true);
+	define('WP_ALLOW_REPAIR', true);
+	@ini_set('display_errors', 0);
+	@ini_set( 'error_reporting', E_ALL ^ E_NOTICE );
+	@ini_set('error_log', dirname(__FILE__) . '/app/php-errors.log');
+}
+
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 require_once(ABSPATH . 'wp-settings.php');
